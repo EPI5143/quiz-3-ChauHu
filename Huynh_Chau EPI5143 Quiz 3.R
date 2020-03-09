19b84c1f509d370e65223338ef5a6c2f4c88c9ee
Chau <chuyn097@uottawa.ca>
2020-03-09 10:29:15 -0400

    Huynh_Chau EPI5143 Quiz 3.R file

---
+++ Huynh_Chau EPI5143 Quiz 3.R
@@ -0,0 +1,33 @@
+#Chau Huynh, 11/03/2020 Quiz 3
+
+install.packages("tidyverse")
+library(tidyverse)
+
+# Question 1: The dataset mpg dataset is a base R dataset which includes data on fuel efficiency of a number
+# of makes and models of automobile Have a look at this dataset using the View() command.
+# how many observations and how many variables does this dataset have? (provide the code 
+# and result from the console window) (hint:use the nrow() and ncol() and/or the dim() R functions)
+view(mpg) 
+nrow(mpg) 
+ncol(mpg) 
+dim(mpg)
+# There are 234 observations and 11 variables. 
+# Nrow=234, Ncol=11, Dim=234 11
+
+ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy))
+# Question 2. Modify and run the ggplot code to make each class of vehicle a different colour
+ggplot (data=mpg) + geom_point(mapping = aes(x=displ, y=hwy, color=class)) 
+
+# Question 3. Further modify and run the code to use a different shape to plot vehicles
+# according whether vehicle is front, rear or 4 wheel drive (drv)
+ggplot (data=mpg) + geom_point(mapping = aes(x=displ, y=hwy, color=class, shape=drv)) 
+
+# Question 4. Further modify and run the code to make the size of each point on the plot proportional
+# to the number of cylinders the vehicle's engine has (cyl)
+ggplot (data=mpg) + geom_point(mapping = aes(x=displ, y=hwy, color=class, shape=drv, size=cyl))
+
+# Question 5. Modify the code to add a suitable title of your choice to your plot
+ggplot (data=mpg) + geom_point(mapping = aes(x=displ, y=hwy, color=class, shape=drv, size=cyl))
+ggtitle("Comparing highway fuel efficiency vs. engine size for vehicles")
+
+
